2fbc9c95c521441953849ae8c0a47d8474039c54
Thinura <thinuralaksara@gmail.com>
2022-10-29 03:05:53 +0530

    added request validator

---
+++ server/middlewares/validateDto.js
@@ -0,0 +1,17 @@
+const ApiError = require('../errors/apiError');
+
+function validateDto(schema) {
+  return async (req, res, next) => {
+    try {
+      const validatedBody = await schema.validate(req.body);
+      // replace request body with validated schema object
+      // so that default values are applied to the DTO
+      req.body = validatedBody;
+      next();
+    } catch (err) {
+      next(ApiError.badRequest(err));
+    }
+  };
+}
+
+module.exports = validateDto;
