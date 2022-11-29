08f463ac0522bab42c7107e5769003cb9128d087
Thinura <thinuralaksara@gmail.com>
2022-11-29 12:54:41 +0530

    updated gridlist test

--- client/components/__tests__/employeeGridList.test.jsx
+++ client/components/__tests__/employeeGridList.test.jsx
@@ -1,38 +1,60 @@
 import { render, screen } from '@testing-library/react';
 import { EmployeeGridList } from '@/components';
 import { EMPLOYEE_LIST_EMPTY_MESSAGE } from '@/constants';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 
 import { mockEmployees } from '../../__mocks__/mockData';
 
+const middlewares = []
+const mockStore = configureStore(middlewares)
+
 describe('Employee Gird List Component', () => {
-
-    it('should render empty employee message for empty employee array',  () => {
+    it('should render empty employee message for empty employee array', () => {
         const emptyEmployees = [];
+        const initStore = {
+            fetchedEmployees: {
+                employees: emptyEmployees,
+            }
+        };
+        const store = mockStore(initStore)
         render(
-            <EmployeeGridList
-                employees={emptyEmployees}
-            />
+            <Provider store={store}>
+                <EmployeeGridList />
+            </Provider>
         );
         const emptyMessageElement = screen.getByText(EMPLOYEE_LIST_EMPTY_MESSAGE);
         expect(emptyMessageElement).toBeInTheDocument();
     });
 
-    it('should render empty employee message for undefined employee array',  () => {
+    it('should render empty employee message for undefined employee array', () => {
         const emptyEmployees = undefined;
+        const initStore = {
+            fetchedEmployees: {
+                employees: emptyEmployees,
+            }
+        };
+        const store = mockStore(initStore)
         render(
-            <EmployeeGridList
-                employees={emptyEmployees}
-            />
+            <Provider store={store}>
+                <EmployeeGridList />
+            </Provider>
         );
         const emptyMessageElement = screen.getByText(EMPLOYEE_LIST_EMPTY_MESSAGE);
         expect(emptyMessageElement).toBeInTheDocument();
     });
 
-    it('should render employee card for employee profile',  () => {
+    it('should render employee card for employee profile', () => {
+        const initStore = {
+            fetchedEmployees: {
+                employees: mockEmployees,
+            }
+        };
+        const store = mockStore(initStore)
         render(
-            <EmployeeGridList
-                employees={mockEmployees}
-            />
+            <Provider store={store}>
+                <EmployeeGridList />
+            </Provider>
         );
         expect(screen.getByText(`${mockEmployees[0].email}`)).toBeInTheDocument();
     });
